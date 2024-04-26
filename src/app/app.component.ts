b6627d59fdcc91b8beb593473410627ea50b08d1
Amycollebarry <amycolle.barry@unchk.edu.sn>
2024-04-26 10:43:30 +0100

    formulaire de connexion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'testBakeli';
+export class LoginFormComponent implements OnInit {
+
+
+  // Propriétés pour stocker les valeurs du formulaire
+
+  email!: string;
+
+  password!: string;
+
+
+  constructor() { }
+
+
+  ngOnInit() {
+
+  }
+
+
+  onSubmit() {
+
+    // Soumettre le formulaire et envoyer les données
+
+    console.log('Formulaire soumis !');
+
+  }
+
 }
